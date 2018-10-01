9815f3882c3cf44cc7f0a194660b37e5f78642bd
Dan Herlihy <djherls@gmail.com>
2018-09-30 22:08:43 -0400

    Include hyphens in URL definition

--- background.js
+++ background.js
@@ -2,11 +2,11 @@ var redirectUrl;
 
 function search(url){
 
-	google_reg = /www.google.com\/(?:search|webhp|)(?:[\w\d.&\?=#+\-]*)\b#?q=((?:[\w%.]+\+?)+)/gi;
-	bing_reg = /www.bing.com\/search(?:[\w\d.&\?=#+\-]*)\?q=((?:[\w%.]+\+?)+)/gi;
-	yahoo_reg = /search.yahoo.com\/search;?(?:[\w\d.&\?=#+\-]*)(?:\?|&)p=((?:[\w%.]+\+?)+)/gi;
-	duck_reg = /duckduckgo.com\/(?:[\w\d.&\?=#+\-]*)?q=((?:[\w%.]+\+?)+)/gi;
-	aol_reg = /search.aol.com\/aol\/search(?:[\w\d.&\?=#+\-]*)(?:\?|&)q=((?:[\w%.]+\+?)+)/gi;
+	google_reg = /www.google.com\/(?:search|webhp|)(?:[\w\d.&\?=#+\-]*)\b#?q=((?:[\w%.\-]+\+?)+)/gi;
+	bing_reg = /www.bing.com\/search(?:[\w\d.&\?=#+\-]*)\?q=((?:[\w%.\-]+\+?)+)/gi;
+	yahoo_reg = /search.yahoo.com\/search;?(?:[\w\d.&\?=#+\-]*)(?:\?|&)p=((?:[\w%.\-]+\+?)+)/gi;
+	duck_reg = /duckduckgo.com\/(?:[\w\d.&\?=#+\-]*)?q=((?:[\w%.\-]+\+?)+)/gi;
+	aol_reg = /search.aol.com\/aol\/search(?:[\w\d.&\?=#+\-]*)(?:\?|&)q=((?:[\w%.\-]+\+?)+)/gi;
 	if (url.includes("google.com")){
 		reg = google_reg;
 	} else if (url.includes("bing.com")){
@@ -25,7 +25,7 @@ function search(url){
 	if (m){
 		search_string = m[1].replace("%20","+");
 		redirectUrl = "http://www.goodsearch.com/search-web?utf8=%E2%9C%93&keywords="+search_string;
-		tabs = chrome.tabs.query({url: '*://*.goodsearch.com/*'}, updateTabs);
+		tabs = chrome.tabs.query({url: '*://search.yahoo.com/yhs/search?*'}, updateTabs);
 	}
 
 
